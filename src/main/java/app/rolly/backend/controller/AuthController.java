fbf9dca92917fa1a4751440c05976e15b1e46128
veronica <weronika.bucewka@wp.pl>
2025-09-24 21:34:55 +0200

    Database connection

--- src/main/java/app/rolly/backend/controller/AuthController.java
+++ src/main/java/app/rolly/backend/controller/AuthController.java
@@ -1,4 +1,15 @@
 package app.rolly.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
+    @GetMapping(value = "/{msg}")
+    public String f(@PathVariable String msg){
+        return msg;
+    }
 }
--- src/main/java/app/rolly/backend/controller/EventController.java
+++ src/main/java/app/rolly/backend/controller/EventController.java
@@ -1,4 +1,9 @@
 package app.rolly.backend.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/event")
 public class EventController {
 }
